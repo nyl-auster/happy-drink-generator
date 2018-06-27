a981f28dfffd81203a25c1d0f3182493e12638c1
yann <yann.boisselier@gmail.com>
2018-06-27 15:41:23 +0200

    test deployment

--- src/data/tweets.js
+++ src/data/tweets.js
@@ -2,5 +2,9 @@ export default [
   {
     message:
       "#happydev est un collectif de #freelances du numérique réuni·e·s autour de valeurs fortes 🔗https://happy-dev.fr/manifeste"
+  },
+  {
+    message:
+      "Freelance ? Plus forts ensemble et sans plateforme centralisatrice : viens découvrir le réseau @HappyDev_Nantes pour travailler autrement :) amène qui tu veux "
   }
 ];
